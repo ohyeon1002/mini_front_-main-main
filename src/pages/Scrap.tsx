f7e0f99cf80e6f8dd0750b812809c8b0a3af0303
yull222 <yurilee4942@gmail.com>
2025-06-20 13:34:26 +0900

    ㅋㅋ화살표 삭제

--- src/pages/Scrap.tsx
+++ src/pages/Scrap.tsx
@@ -85,7 +85,7 @@ export default function Scrap() {
           onClick={() => navigate(-1)}
           className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400"
         >
-          ← 뒤로가기
+          뒤로가기
         </button>
       </div>
 
