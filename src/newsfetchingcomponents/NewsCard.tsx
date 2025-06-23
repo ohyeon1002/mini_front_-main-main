48096ec1fe8a2be3cecc1545466ed27cb196e03e
ohyeon <skyblueazure4@gmail.com>
2025-06-24 01:20:03 +0900

    title, imagesize, display

--- src/newsfetchingcomponents/NewsCard.tsx
+++ src/newsfetchingcomponents/NewsCard.tsx
@@ -147,7 +147,7 @@ export default function NewsCard({ data, thumbnail }: dataProps) {
       {/* 상단 내용 영역 */}
       <div className="h-full">
         <div className="h-3/7 rounded-lg overflow-hidden">
-          <img src={thumbnail} className="h-full object-cover" />
+          <img src={thumbnail} className="w-full h-full object-cover" />
         </div>
         <h2 className="text-lg font-semibold text-gray-800 mb-2">
           <a
