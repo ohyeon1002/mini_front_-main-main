317a338e59138b60501dca1dc9c450cc1d3bc4db
ohyeon <skyblueazure4@gmail.com>
2025-06-24 01:16:32 +0900

    ofhidden

--- src/newsfetchingcomponents/NewsCard.tsx
+++ src/newsfetchingcomponents/NewsCard.tsx
@@ -146,8 +146,8 @@ export default function NewsCard({ data, thumbnail }: dataProps) {
     <div className="bg-white shadow-md rounded-lg p-4 m-2 flex flex-col justify-between h-full">
       {/* 상단 내용 영역 */}
       <div className="h-full">
-        <div className="h-1/3 rounded-lg">
-          <img src={thumbnail} className="w-full h-full object-cover" />
+        <div className="h-3/7 rounded-lg overflow-hidden">
+          <img src={thumbnail} className="h-full object-cover" />
         </div>
         <h2 className="text-lg font-semibold text-gray-800 mb-2">
           <a
