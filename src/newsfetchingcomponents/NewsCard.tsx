9307d4d010b31ffb56213d0c3db54b13d5de01f7
ohyeon <skyblueazure4@gmail.com>
2025-06-24 00:58:56 +0900

    objectfit again

--- src/newsfetchingcomponents/NewsCard.tsx
+++ src/newsfetchingcomponents/NewsCard.tsx
@@ -147,7 +147,7 @@ export default function NewsCard({ data, thumbnail }: dataProps) {
       {/* 상단 내용 영역 */}
       <div>
         <div className="max-h-1/3 rounded-lg">
-          <img src={thumbnail} object-fit="cover"/>
+          <img src={thumbnail} style={{objectFit:'cover'}} />
         </div>
         <h2 className="text-lg font-semibold text-gray-800 mb-2">
           <a
