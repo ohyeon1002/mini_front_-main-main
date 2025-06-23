7f64917fd341c2dda2f42738a2d17521b3efa12a
ohyeon <skyblueazure4@gmail.com>
2025-06-24 01:11:09 +0900

    dasi

--- src/newsfetchingcomponents/NewsCard.tsx
+++ src/newsfetchingcomponents/NewsCard.tsx
@@ -146,7 +146,7 @@ export default function NewsCard({ data, thumbnail }: dataProps) {
     <div className="bg-white shadow-md rounded-lg p-4 m-2 flex flex-col justify-between h-full">
       {/* 상단 내용 영역 */}
       <div>
-        <div className="max-h-1/3 rounded-lg">
+        <div className="h-1/3 rounded-lg">
           <img src={thumbnail} className="w-full h-full object-cover" />
         </div>
         <h2 className="text-lg font-semibold text-gray-800 mb-2">
